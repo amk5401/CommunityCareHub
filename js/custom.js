8e513694a7460751ed8853e0ade04b9d09849220
Austin Klein <amk5401@rit.edu>
2016-04-05 18:56:48 -0400

    Initial Commit with real content

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+var app = angular.module('communityCareHubApp', [])
+    .controller('communityCareHubController', function ($scope, $window) {
+        $scope.submitLogin = function () {
+            if ($scope.username === 'Caregiver' && $scope.password === 'abc123') {
+                $window.location.href = '/src/caregiverHome.html';
+            }
+            else {
+                alert("Incorrect Username or Password");
+            }
+        };
+
+    });
