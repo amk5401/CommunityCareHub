965f0954b171a6849fb19e6142add451908f3992
Giovanni Carvelli <giovannircarvelli@gmail.com>
2016-04-10 19:01:10 -0400

    Add flash for bad login instead of javascript alert

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
     <form ng-submit="submitLogin()" ng-controller="communityCareHubController" class="col-md-4 col-md-offset-4 mid-page center-text">
         <h1>Community Care Hub</h1>
-        <br />
+        <div id="flash">
+
+        </div>
         <div class="input-group input-group-lg">
             <span class="input-group-addon" id="username-field">Username</span>
             <input type="text" ng-model="username" class="form-control" aria-describedby="username-field">
@@ -14,7 +16,7 @@
         </div>
         <br />
         <div class="btn-group btn-group-lg" role="group" aria-label="login-button">
-            <button type="submit" class="btn btn-success">Login</button>
+            <button type="submit" class="btn btn-success" id="login-button">Login</button>
         </div>
     </form>
 
--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,26 @@
 var app = angular.module('communityCareHubApp', [])
     .controller('communityCareHubController', function ($scope, $window) {
         $scope.submitLogin = function () {
-            if ($scope.username === 'Caregiver' && $scope.password === 'abc123') {
-                $window.location.href = '/caregiverHome.php';
-            }
-            else {
-                alert("Incorrect Username or Password");
-            }
+            $scope.clearFlash();
+            $("#login-button").prop("disabled", true);
+            // wait a little to give the impression of it loading
+            setTimeout(function() {
+                if ($scope.username === 'Caregiver' && $scope.password === 'abc123') {
+                    $window.location.href = '/caregiverHome.php';
+                }
+                else {
+                    $scope.flash("Incorrect username or password", "danger");
+                    $("#login-button").prop("disabled", false);
+                }
+            }, 1000);
+        };
+
+        $scope.flash = function(message, severity) {
+            $("#flash").addClass("alert alert-" + severity).html(message);
+        };
+
+        $scope.clearFlash = function() {
+            $("#flash").html("").removeClass();
         };
 
     });
