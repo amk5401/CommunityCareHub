b1d1cb881355c5b6ef0d3b0c91d83022821f8054
Giovanni Carvelli <giovannircarvelli@gmail.com>
2016-04-11 20:36:00 -0400

    Actually implemented calendar modal

--- modals/addCalendarModal.php
+++ modals/addCalendarModal.php
@@ -5,13 +5,58 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Modal Header</h4>
+                <h4 class="modal-title">Add a Calendar Event</h4>
             </div>
             <div class="modal-body">
-                <p>Some text in the modal.</p>
+                <form ng-submit="newCalendarEvent()" class="form-horizontal">
+                    <div class="form-group">
+                        <label for="event-name" class="col-md-3 control-label">Event Name</label>
+                        <div class="col-md-7">
+                            <input id="event-name" type="text" ng-model="eventName" class="form-control" aria-describedby="event-field">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="event-start-day" class="col-md-3 control-label">Start</label>
+                        <div class="col-md-3">
+                            <input id="event-start-day" type="text" ng-model="eventStartDay" class="form-control" aria-describedby="event-field">
+                        </div>
+                        <div class="col-md-2">
+                            <input id="event-start-time" type="text" ng-model="eventStartTime" class="form-control" aria-describedby="event-field">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="event-end-day" class="col-md-3 control-label">End</label>
+                        <div class="col-md-3">
+                            <input id="event-end-day" type="text" ng-model="eventEndDay" class="form-control" aria-describedby="event-field">
+                        </div>
+                        <div class="col-md-2">
+                            <input id="event-end-time" type="text" ng-model="eventEndTime" class="form-control" aria-describedby="event-field">
+                        </div>
+                    </div>
+                    <div class="form-border">
+                        <div class="row">
+                            <div id="addCalendarEvent-detailLabel" class="col-md-12 center-text">
+                                Additional Details
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="event-location" class="col-md-3 control-label">Where</label>
+                            <div class="col-md-7">
+                                <input id="event-location" type="text" ng-model="eventLocation" class="form-control" aria-describedby="event-field">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="event-description" class="col-md-3 control-label">Description</label>
+                            <div class="col-md-7">
+                                <textarea id="event-description" ng-model="eventDescription" class="form-control" rows="3"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Add Event</button>
             </div>
         </div>
 
--- staffCalendar.php
+++ staffCalendar.php
@@ -5,7 +5,7 @@
 </div>
 
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-6 col-md-offset-3" id="addCalendarEvent-button">
         <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#addCalendarEvent-modal">Open Modal</button>
     </div>
 </div>
