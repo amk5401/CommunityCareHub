bd44f6778adce67326d4e953e34941243dca0b3e
Giovanni Carvelli <giovannircarvelli@gmail.com>
2016-04-14 15:14:35 -0400

    small change to login form, don't really remember honestly

--- index.php
+++ index.php
@@ -5,19 +5,21 @@
         <div id="flash">
 
         </div>
-        <div class="input-group input-group-lg">
-            <span class="input-group-addon" id="username-field">Username</span>
-            <input type="text" ng-model="username" class="form-control" aria-describedby="username-field">
-        </div>
-        <br />
-        <div class="input-group input-group-lg">
-            <span class="input-group-addon" id="password-field">Password</span>
-            <input type="password" ng-model="password" class="form-control" aria-describedby="password-field">
-        </div>
-        <br />
-        <div class="btn-group btn-group-lg" role="group" aria-label="login-button">
-            <button type="submit" class="btn btn-success" id="login-button">Login</button>
-        </div>
+        <form class="form-horizontal">
+            <div class="input-group input-group-lg">
+                <span class="input-group-addon" id="username-field">Username</span>
+                <input type="text" ng-model="username" class="form-control" aria-describedby="username-field">
+            </div>
+            <br />
+            <div class="input-group input-group-lg">
+                <span class="input-group-addon" id="password-field">Password</span>
+                <input type="password" ng-model="password" class="form-control" aria-describedby="password-field">
+            </div>
+            <br />
+            <div class="btn-group btn-group-lg" role="group" aria-label="login-button">
+                <button type="submit" class="btn btn-success" id="login-button">Login</button>
+            </div>
+        </form>
     </form>
 
 <?php include 'footer.php' ?>
