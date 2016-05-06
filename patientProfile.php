6048e1ee08f1eda82ec8eb74c3ff526a63fb81fb
Aidan Sawyer <aks5238@rit.edu>
2016-05-06 01:56:25 +0000

    change tab labels to h3 for improved readability

--- patientProfile.php
+++ patientProfile.php
@@ -1,15 +1,15 @@
 <?php include 'header.php' ?>
     <ul class="nav nav-tabs nav-justified">
-        <li role="presentation" class="active"><a data-toggle="tab" href="#Medical">Medical</a></li>
-        <li role="presentation"><a data-toggle="tab" href="#General">General</a></li>
-        <li role="presentation"><a data-toggle="tab" href="#Personal">Personal</a></li>
+        <li role="presentation"><a data-toggle="tab" href="#Medical"><h4>Medical</h4></a></li>
+        <li role="presentation" class="active"><a data-toggle="tab" href="#General"><h4>General</h4></a></li>
+        <li role="presentation"><a data-toggle="tab" href="#Personal"><h4>Personal</h4></a></li>
     </ul>
 
     <div class="tab-content">
-        <div class="tab-pane active" id="Medical">
+        <div class="tab-pane" id="Medical">
             <?php include 'medical.php' ?>
         </div>
-        <div class="tab-pane" id="General">
+        <div class="tab-pane active" id="General">
             <?php include 'general.php' ?>
         </div>
         <div class="tab-pane" id="Personal">
